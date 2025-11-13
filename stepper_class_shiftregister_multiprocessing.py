d64474cfed729337910e68382bc1462f0975e8cb
mcr2610 <mcr.2610@gmail.com>
2025-11-13 09:05:18 -0500

    Update stepper_class_shiftregister_multiprocessing.py

--- stepper_class_shiftregister_multiprocessing.py
+++ stepper_class_shiftregister_multiprocessing.py
@@ -109,11 +109,14 @@ if __name__ == '__main__':
     m2.zero()
     m1.goAngle(90)
     m1.goAngle(-45)
-    m2.goAngle(-90)
-    m2.goAngle(45)
     m1.goAngle(-135)
     m1.goAngle(135)
     m1.goAngle(0)
+    m2.goAngle(90)
+    m2.goAngle(-45)
+    m2.goAngle(-135)
+    m2.goAngle(135)
+    m2.goAngle(0)
     # While the motors are running in their separate processes, the main
     # code can continue doing its thing:
     try:
